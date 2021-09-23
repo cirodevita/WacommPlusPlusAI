a96f5a819f323206225460f0a47961264903882d
cirodevita <Anomalo97>
2021-09-23 17:36:02 +0200

    rewriting code time series

--- time_series.py
+++ time_series.py
@@ -1,56 +1,24 @@
-import matplotlib.pyplot as plt
-import datetime
-import netCDF4
-import numpy as np
-from configparser import ConfigParser
-from shapely.geometry import Polygon, Point
+import concurrent.futures
+import multiprocessing
+import time
 import calendar
+from configparser import ConfigParser
+from datetime import datetime, timedelta
+from functools import partial
 from pathlib import Path
 
-from main import load_areas, remove_measures_duplicates
+import netCDF4
+import numpy as np
+from matplotlib import pyplot as plt
+from shapely.geometry import Polygon
+
+from main import load_areas, remove_measures_duplicates, get_lat_long, get_index_lat_long, getMaxConc
 
 cfg = ConfigParser()
 cfg.read('config.ini')
 
-areas = load_areas()
-max_measures = remove_measures_duplicates()
 
-time_series = []
-for area in areas:
-    time_series.append({"name": area['properties']['DENOMINAZI'], "datetime": [], "values": [],
-                        "datetime_measures": [], "measures": []})
-
-_year = 2019
-_month = 1
-_day = 1
-
-_hours = 8760
-
-file = open("test/url_mancanti.txt", "a")
-
-
-def get_index_lat_long(lat, long, min_lat, max_lat, min_long, max_long):
-    N = len(lat)
-    sum_delta_lat = 0
-    for i in range(1, N):
-        sum_delta_lat += lat[i] - lat[i - 1]
-    delta_lat = sum_delta_lat / N
-
-    M = len(long)
-    sum_delta_long = 0
-    for i in range(1, M):
-        sum_delta_long += long[i] - long[i - 1]
-    delta_long = sum_delta_long / M
-
-    index_min_lat = round((min_lat - lat[0]) / delta_lat)
-    index_max_lat = round((max_lat - lat[0]) / delta_lat)
-    index_min_long = round((min_long - long[0]) / delta_long)
-    index_max_long = round((max_long - long[0]) / delta_long)
-
-    return index_min_lat, index_max_lat, index_min_long, index_max_long
-
-
-def measure(index, date, area):
+def measure(index, date, reference_hour, area, max_measures):
     if any((d['datetime'] == date) and (
             (d['site_name'].replace(" ", "")) == (area['properties']['DENOMINAZI'].replace(" ", ""))) for d in
            max_measures):
@@ -62,133 +30,152 @@ def measure(index, date, area):
         time_series[index]['measures'].append(int(max_measures[_index]['outcome']))
 
 
-for i in range(0, _hours + 1):
-    reference_hour = datetime.datetime(_year, _month, _day, 0, 0) + datetime.timedelta(hours=i)
+def worker(year, areas, lat, long, delta_lat, delta_long, max_measures, time_series, i):
+    reference_hour = datetime(year, 1, 1, 0, 0) + timedelta(hours=i)
     formatted_hour = reference_hour.strftime("%Y%m%dZ%H%M")
 
-    year = str(reference_hour.year)
-    month = f'{reference_hour.month:02}'
-    day = f'{reference_hour.day:02}'
-    hour = f'{reference_hour.hour:02}'
+    _year = str(reference_hour.year)
+    _month = f'{reference_hour.month:02}'
+    _day = f'{reference_hour.day:02}'
+    _hour = f'{reference_hour.hour:02}'
 
-    url = cfg.get('variables', 'URL') + year + "/" + month + "/" + day + "/wcm3_d03_" + formatted_hour + ".nc"
+    date = _day + "/" + _month + "/" + _year[2:4] + "/" + _hour + ":00"
 
-    date = day + "/" + month + "/" + year[2:4] + "/" + hour + ":00"
+    for area in areas:
+        bbox = area['bbox']
+        min_long = bbox[0]
+        min_lat = bbox[1]
+        max_long = bbox[2]
+        max_lat = bbox[3]
 
-    try:
-        dataset = netCDF4.Dataset(url)
-        long = dataset.variables['longitude']
-        lat = dataset.variables['latitude']
+        coordinates = area['geometry']['coordinates'][0][0]
+        area_poly = Polygon(coordinates)
 
-        concentration = dataset.variables['conc'][0]
+        index = [i for i, _ in enumerate(time_series) if
+                 (_['name']).replace(" ", "") == (area['properties']['DENOMINAZI']).replace(" ", "")][0]
 
-        for area in areas:
-            bbox = area['bbox']
+        time_series[index]['datetime'].append(reference_hour)
 
-            coordinates = area['geometry']['coordinates'][0][0]
-            area_poly = Polygon(coordinates)
+        index_min_lat, index_max_lat, index_min_long, index_max_long = get_index_lat_long(lat, long,
+                                                                                          delta_lat, delta_long,
+                                                                                          min_lat, max_lat,
+                                                                                          min_long, max_long)
 
-            index = [i for i, _ in enumerate(time_series) if
-                     (_['name']).replace(" ", "") == (area['properties']['DENOMINAZI']).replace(" ", "")][0]
+        url = cfg.get('variables', 'URL') + _year + "/" + _month + "/" + _day + "/wcm3_d03_" + formatted_hour + \
+              ".nc?conc[0:1:0][0:1:1][" + str(index_min_lat) + ":1:" + str(index_max_lat) + "][" + \
+              str(index_min_long) + ":1:" + str(index_max_long) + "]"
 
-            time_series[index]['datetime'].append(reference_hour)
+        # max = getMaxConc(url, lat, long, index_min_lat, index_min_long, area_poly)
+        max = np.random.randint(300)
 
-            index_min_lat, index_max_lat, index_min_long, index_max_long = get_index_lat_long(lat, long,
-                                                                                              bbox[1], bbox[3],
-                                                                                              bbox[0], bbox[2])
+        if max == "NaN":
+            max = 0
+        time_series[index]['values'].append(int(max))
 
-            max = np.float32("-inf")
-            for k in range(0, 2):
-                for i in range(index_min_lat, index_max_lat + 1):
-                    for j in range(index_min_long, index_max_long + 1):
-                        point = Point(long[j], lat[i])
-                        if point.within(area_poly):
-                            value = concentration[k][i][j]
-                            if value > max:
-                                max = value
+        measure(index, date, reference_hour, area, max_measures)
 
-            if max == "NaN":
-                max = 0
-            time_series[index]['values'].append(int(max))
 
-            measure(index, date, area)
+def create_timeseries(year, areas, lat, long, delta_lat, delta_long, max_measures, time_series, hours):
+    """
+    workers = multiprocessing.cpu_count()
+    func = partial(worker, year, areas, lat, long, delta_lat, delta_long, max_measures, time_series)
+    with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
+        [executor.submit(func, i) for i in range(0, hours)]
+    """
+    for i in range(0, hours + 1):
+        worker(year, areas, lat, long, delta_lat, delta_long, max_measures, time_series, i)
 
-            print(area['properties']['DENOMINAZI'], reference_hour)
-    except:
-        for area in areas:
-            index = [i for i, _ in enumerate(time_series) if
-                     (_['name']).replace(" ", "") == (area['properties']['DENOMINAZI']).replace(" ", "")][0]
 
-            time_series[index]['datetime'].append(reference_hour)
-            time_series[index]['values'].append(0)
+def create_graphics(time_series):
+    for time in time_series:
+        directory_name = "graphics/" + (time["name"]).replace("/", "")
+        Path(directory_name).mkdir(parents=True, exist_ok=True)
 
-            measure(index, date, area)
+        hours = []
+        measures = []
 
-            print(area['properties']['DENOMINAZI'], reference_hour)
+        temp_month = 1
+        temp_count = 0
+        index_start = 0
 
-file.close()
+        for t in time['datetime']:
+            if temp_month == int(t.month):
+                year = t.year
+                hours.append(t)
+            else:
+                x = np.array(hours)
+                index_end = index_start + len(x)
+                y = np.array(time['values'][index_start:index_end])
 
-for time in time_series:
-    directory_name = "graphics/" + (time["name"]).replace("/", "")
-    Path(directory_name).mkdir(parents=True, exist_ok=True)
+                count = 0
+                for m in time['datetime_measures']:
+                    if temp_month == m.month:
+                        measures.append(m)
+                        count += 1
 
-    hours = []
-    measures = []
+                x1 = np.array(measures)
+                y1 = np.array(time['measures'][temp_count:temp_count + count])
 
-    temp_month = 1
-    temp_count = 0
-    index_start = 0
+                temp_count += count
 
-    for t in time['datetime']:
-        if temp_month == int(t.month):
-            year = t.year
-            hours.append(t)
-        else:
-            x = np.array(hours)
-            index_end = index_start + len(x)
-            y = np.array(time['values'][index_start:index_end])
+                fig = plt.figure()
+                fig.set_size_inches(10, 5.0)
+                fig.suptitle(time['name'] + ": " + calendar.month_name[int(temp_month)] + " " + str(year), fontsize=20)
 
-            count = 0
-            for m in time['datetime_measures']:
-                if temp_month == m.month:
-                    measures.append(m)
-                    count += 1
+                ax = fig.add_subplot()
+                ax.plot(x, y, linewidth=0.2, color='black')
+                ax.axhline(y=67, color='b', linestyle='-', linewidth=0.2)
+                ax.axhline(y=230, color='b', linestyle='-', linewidth=0.2)
+                ax.set_ylim(0, 520)
+                ax.set_xlabel("Time [h]")
+                ax.set_ylabel("number of particles")
+                ax.set_xticks([x[0], x[-1]])
+                ax.set_yticks([67, 230, 500])
 
-            x1 = np.array(measures)
-            y1 = np.array(time['measures'][temp_count:temp_count + count])
+                ax2 = ax.twinx()
+                ax2.bar(x1, y1, width=0.2, color='red')
+                ax2.set_ylim(0, 520)
+                ax2.set_ylabel("E. coil/100g FIL")
+                ax.set_xticks([x[0], x[-1]])
+                ax2.set_yticks([67, 230, 500])
 
-            temp_count += count
+                if len(x1) != 0:
+                    for k in x1:
+                        plt.fill_between(x, 0, y, color='green',
+                                         where=(x < k) & (x > k - timedelta(hours=168)))
 
-            fig = plt.figure()
-            fig.set_size_inches(10, 5.0)
-            fig.suptitle(time['name'] + ": " + calendar.month_name[int(temp_month)] + " " + str(year), fontsize=20)
+                fig.savefig(directory_name + "/" + str(calendar.month_name[int(temp_month)]) + "-" + str(year) + ".png",
+                            dpi=100)
 
-            ax = fig.add_subplot()
-            ax.plot(x, y, linewidth=0.2, color='black')
-            ax.axhline(y=67, color='b', linestyle='-', linewidth=0.2)
-            ax.axhline(y=230, color='b', linestyle='-', linewidth=0.2)
-            ax.set_ylim(0, 520)
-            ax.set_xlabel("Time [h]")
-            ax.set_ylabel("number of particles")
-            ax.set_xticks([x[0], x[-1]])
-            ax.set_yticks([67, 230, 500])
+                hours.clear()
+                measures.clear()
 
-            ax2 = ax.twinx()
-            ax2.bar(x1, y1, width=0.2, color='red')
-            ax2.set_ylim(0, 520)
-            ax2.set_ylabel("E. coil/100g FIL")
-            ax.set_xticks([x[0], x[-1]])
-            ax2.set_yticks([67, 230, 500])
+                temp_month = int(t.month)
+                index_start += len(x) + 1
 
-            if len(x1) != 0:
-                for k in x1:
-                    plt.fill_between(x, 0, y, color='green', where=(x < k) & (x > k - datetime.timedelta(hours=168)))
 
-            fig.savefig(directory_name + "/" + str(calendar.month_name[int(temp_month)]) + "-" + str(year) + ".png",
-                        dpi=100)
+if __name__ == "__main__":
+    start = time.time()
 
-            hours.clear()
-            measures.clear()
+    areas = load_areas()
+    max_measures = remove_measures_duplicates()
 
-            temp_month = int(t.month)
-            index_start += len(x) + 1
+    time_series = []
+    for area in areas:
+        time_series.append({"name": area['properties']['DENOMINAZI'], "datetime": [], "values": [],
+                            "datetime_measures": [], "measures": []})
+
+    lat, long, delta_lat, delta_long = get_lat_long()
+
+    year = cfg.getint('time_series', 'YEAR')
+    dt = datetime(year, 1, 1, 00, 00, 00)
+    # dt2 = datetime(year + 1, 1, 1, 00, 00, 00)
+    dt2 = datetime(year, 2, 1, 00, 00, 00)
+    hours = (dt2 - dt).days * 24
+    create_timeseries(year, areas, lat, long, delta_lat, delta_long, max_measures, time_series, hours)
+
+    create_graphics(time_series)
+
+    end = time.time()
+
+    print(end - start)
