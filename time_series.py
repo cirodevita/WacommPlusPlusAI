578e38fcfb6e69bd75f494ae270daed9e56ca7f7
cirodevita <Anomalo97>
2021-09-24 20:11:17 +0200

    new graphics (72 hours green, other yellow)

--- time_series.py
+++ time_series.py
@@ -16,7 +16,7 @@ cfg = ConfigParser()
 cfg.read('config.ini')
 
 
-def worker(year, month, hours, lat, long, delta_lat, delta_long, max_measures, area):
+def worker(year, month, day, hours, lat, long, delta_lat, delta_long, max_measures, area):
     bbox = area['bbox']
     min_long = bbox[0]
     min_lat = bbox[1]
@@ -37,7 +37,7 @@ def worker(year, month, hours, lat, long, delta_lat, delta_long, max_measures, a
     print(temp_time_series)
 
     for i in range(0, hours + 1):
-        reference_hour = datetime(year, month, 1, 0, 0) + timedelta(hours=i)
+        reference_hour = datetime(year, month, day, 0, 0) + timedelta(hours=i)
         formatted_hour = reference_hour.strftime("%Y%m%dZ%H%M")
 
         print(area['properties']['DENOMINAZI'], reference_hour)
@@ -56,7 +56,7 @@ def worker(year, month, hours, lat, long, delta_lat, delta_long, max_measures, a
               str(index_min_long) + ":1:" + str(index_max_long) + "]"
 
         value = getConc(url, lat, long, index_min_lat, index_min_long, area_poly)
-        # max = np.random.randint(300)
+        # value = np.random.randint(300)
         if value == "NaN":
             value = 0
         temp_time_series['values'].append(round(float(value), 2))
@@ -80,14 +80,18 @@ def worker(year, month, hours, lat, long, delta_lat, delta_long, max_measures, a
 def create_timeseries(lat, long, delta_lat, delta_long, max_measures, areas):
     year = cfg.getint('time_series', 'YEAR')
     month = cfg.getint('time_series', 'MONTH')
-    dt = datetime(year, month, 1, 00, 00, 00)
-    dt2 = datetime(year + 1, month, 1, 00, 00, 00)
+    day = cfg.getint('time_series', 'DAY')
+    end_year = cfg.getint('time_series', 'END_YEAR')
+    end_month = cfg.getint('time_series', 'END_MONTH')
+    end_day = cfg.getint('time_series', 'END_DAY')
+    dt = datetime(year, month, day, 00, 00, 00)
+    dt2 = datetime(end_year, end_month, end_day, 00, 00, 00)
     hours = (dt2 - dt).days * 24
 
     time_series = []
 
     workers = multiprocessing.cpu_count()
-    func = partial(worker, year, month, hours, lat, long, delta_lat, delta_long, max_measures)
+    func = partial(worker, year, month, day, hours, lat, long, delta_lat, delta_long, max_measures)
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
         for result in executor.map(func, areas):
             time_series.append(result)
@@ -150,9 +154,11 @@ def create_graphics(time_series):
                 ax2.set_yticks([67, 230, 500])
 
                 if len(x1) != 0:
-                    for k in x1:
+                    for k in x1[::-1]:
+                        plt.fill_between(x, 0, y, color='yellow',
+                                         where=(x < k - timedelta(hours=72)) & (x > k - timedelta(hours=168)))
                         plt.fill_between(x, 0, y, color='green',
-                                         where=(x < k) & (x > k - timedelta(hours=168)))
+                                         where=(x < k) & (x > k - timedelta(hours=72)))
 
                 fig.savefig(directory_name + "/" + str(calendar.month_name[int(temp_month)]) + "-" + str(year) + ".png",
                             dpi=100)
